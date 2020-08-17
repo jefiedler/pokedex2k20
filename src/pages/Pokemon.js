899b0417889e6cab350cac2be9c0fc02525ec651
jefiedler <jonas.fiedler@magenta.de>
2020-08-17 12:21:00 +0200

    Add hooks to pokemon.js

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -4,7 +4,9 @@ import { useParams } from 'react-router-dom';
 function Pokemon() {
     const {pokemonName} = useParams();
     const [pokemon, setPokemon] = useState(null);
-    
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+ 
     return (
         <div>
             <p>{pokemonName}</p>
