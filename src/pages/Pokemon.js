2592779149287eb19e1d5105abe46455182e1086
jefiedler <jonas.fiedler@magenta.de>
2020-08-17 12:29:14 +0200

    Add useEffect to pokemon page

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,11 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { fetchPokemon } from '../api/pokemons';
 
 function Pokemon() {
     const {pokemonName} = useParams();
     const [pokemon, setPokemon] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            try{
+                setLoading(true);
+                setError(null);
+                const fetchedPokemon = await fetchPokemon(pokemonName);
+                setPokemon(fetchedPokemon);
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+                setError(error);
+            }
+        }
+        fetchData();
+    }, [pokemonName]);
  
     return (
         <div>
